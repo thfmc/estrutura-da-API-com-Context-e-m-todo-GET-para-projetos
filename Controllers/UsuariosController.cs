65e223c9dcadf316beae54ded4197e6e99e38873
thfmc <128862181+thfmc@users.noreply.github.com>
2023-12-12 21:49:25 -0300

    Criar método de login e CORS

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -3,6 +3,9 @@ using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Exo.WebApi.Controllers
 {
@@ -12,12 +15,10 @@ namespace Exo.WebApi.Controllers
     public class UsuariosController : ControllerBase
     {
          private readonly UsuarioRepository _usuarioRepository;
-
          public UsuariosController(UsuarioRepository usuarioRepository)
         {
              _usuarioRepository = usuarioRepository;
         }
-
 
         // get -> /api/usuarios
         [HttpGet]
@@ -26,29 +27,74 @@ namespace Exo.WebApi.Controllers
              return Ok(_usuarioRepository.Listar());
         }
 
-
         // post -> /api/usuarios
-        [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
+        // [HttpPost]
+        // public IActionResult Cadastrar(Usuario usuario)
+        // {
+        //      _usuarioRepository.Cadastrar(usuario);
+        //      return StatusCode(201);
+        // }
+        // Novo código POST para auxiliar o metódo de login.
+        public IActionResult Post(Usuario usuario)
         {
-             _usuarioRepository.Cadastrar(usuario);
-             return StatusCode(201);
-        }
+            Usuario usuariobuscado = _usuarioRepository.Login(usuario.Email, usuario.Senha); 
+            if (usuariobuscado == null)
+            {
+                return NotFound("E-mail ou senha inválidos!");
+            }
+
+            // Se o usuário for encontrado, segue a criação do token.
+
+            // Define os dados que serão fornecidos no token - Payload.
+            var claims = new[]
+            {
+                // Armazena na claim o e-mail usuário autenticado.
+                new Claim(JwtRegisteredClaimNames.Email, usuariobuscado.Email),
+
+                // Armazena na claim o id do usuário autenticado.
+                new Claim(JwtRegisteredClaimNames.Jti, usuariobuscado.Id.ToString()),
+            };
+
+             // Define a chave de acesso ao token.
+             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chave-autenticacao"));
+
+            // Define as credenciais do token.
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Gera o token.
+            var token = new JwtSecurityToken(
+                issuer: "exoapi.webapi ", // Emissor do token.
+                audience: "exoapi.webapi", // Destinatário do token.
+                claims: claims, // Dados definidos acima.
+                expires: DateTime.Now.AddMinutes(30), // Tempo de expiração.
+                signingCredentials: creds // Credenciais do token.
+            );
+
+            // Retorna ok com o token.
+            return Ok(
+                 new { token = new JwtSecurityTokenHandler(). WriteToken(token) }
+            );
+        }           
+// Fim do novo código POST para auxiliar o método de Login.
+
+
 
         // get -> /api/usuarios/{id}
         [HttpGet("{id}")] // Faz a busca pelo ID.
         public IActionResult BuscarPorId(int id)
         {
             Usuario usuario = _usuarioRepository.BuscaPorId(id);
+#pragma warning disable CS0642 // Instrução empty possivelmente incorreta
             if (usuario == null);
+#pragma warning restore CS0642 // Instrução empty possivelmente incorreta
             {
                 return NotFound();
             }
-            return Ok(usuario);
         }
 
         // put -> /api/usuarios/{id}
         // Atualiza.
+        [Authorize]
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Usuario usuario)
         {
@@ -57,6 +103,7 @@ namespace Exo.WebApi.Controllers
         }
 
         // delete -> /api/usuarios/{id}
+        [Authorize]
         [HttpDelete("{id}")]
         public  IActionResult Deletar(int id)
         {
@@ -65,7 +112,7 @@ namespace Exo.WebApi.Controllers
                 _usuarioRepository.Deletar(id);  
                 return StatusCode (204);
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 return BadRequest();
             }
